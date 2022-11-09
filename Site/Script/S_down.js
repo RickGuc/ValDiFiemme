cde9e798a5ef0de10255fe03664de6cce636bcbf
RickGuc <115940710+RickGuc@users.noreply.github.com>
2022-11-09 18:32:17 +0100

    Add files via upload

---
+++ Site/Script/S_down.js
@@ -0,0 +1,12 @@
+
+
+
+document.getElementById("men").onclick = function() {dog()};
+function dog() {
+  document.getElementById("drop").classList.toggle("show");
+}
+
+document.getElementById("menbis").onclick = function() {myFunction()};
+function myFunction() {
+  document.getElementById("dropbis").classList.toggle("shown");
+}
